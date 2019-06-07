59c245bb7711f1beab1c2c266cf00658f9310710
Legkostup Andrey <redginger979gmail.com>
2019-06-07 11:25:56 +0300

    Initial commit

---
+++ app/src/main/java/com/example/spinner/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.spinner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class  MainActivity extends AppCompatActivity {
+    private Spinner spinner;
+    private TextView textView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        spinner = findViewById(R.id.spinner);
+        textView = findViewById(R.id.textView);
+    }
+
+    public void showTemp(View view) {
+        int position = spinner.getSelectedItemPosition();
+        String description = getDescriptionByPosition(position);
+        textView.setText(description);
+    }
+    private String getDescriptionByPosition(int position){
+        String[] descriptions = getResources().getStringArray(R.array.temp);
+        return  descriptions[position];
+    }
+}
